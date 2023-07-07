2b64ede24ff5b2b5aec719086bdb88cd3f56bf0c
rajalakshmi@289 <rajalakshmi.e6220005@sret.edu.in>
2023-07-07 18:54:17 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,231 @@
+from flask import Flask, flash, request, redirect, render_template
+import os
+from werkzeug.utils import secure_filename
+import cv2
+import pickle
+import imutils
+from keras.models import load_model
+import numpy as np
+from keras.applications.vgg16 import preprocess_input
+import nltk
+nltk.download('popular')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+model = load_model('model.h5')
+import json
+import random
+
+intents = json.loads(open('data.json').read())
+words = pickle.load(open('texts.pkl', 'rb'))
+classes = pickle.load(open('labels.pkl', 'rb'))
+
+def clean_up_sentence(sentence):
+
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+
+def bow(sentence, words, show_details=True):
+
+    sentence_words = clean_up_sentence(sentence)
+
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+
+def predict_class(sentence, model):
+
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+
+alzheimer_model = load_model('models/alzheimer_model.h5')
+heart_model = pickle.load(open('models/heart_disease.pickle.dat', "rb"))
+
+
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "secret key"
+
+############################################# BRAIN TUMOR FUNCTIONS ################################################
+
+
+def preprocess_imgs(set_name, img_size):
+    """
+    Resize and apply VGG-15 preprocessing
+    """
+    set_new = []
+    for img in set_name:
+        img = cv2.resize(img, dsize=img_size, interpolation=cv2.INTER_CUBIC)
+        set_new.append(preprocess_input(img))
+    return np.array(set_new)
+
+
+def crop_imgs(set_name, add_pixels_value=0):
+    """
+    Finds the extreme points on the image and crops the rectangular out of them
+    """
+    set_new = []
+    for img in set_name:
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        # threshold the image, then perform a series of erosions + dilations to remove any small regions of noise
+        thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
+        # find contours in thresholded image, then grab the largest one
+        cnts = cv2.findContours(
+            thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        c = max(cnts, key=cv2.contourArea)
+
+        # find the extreme points
+        extLeft = tuple(c[c[:, :, 0].argmin()][0])
+        extRight = tuple(c[c[:, :, 0].argmax()][0])
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+        ADD_PIXELS = add_pixels_value
+        new_img = img[extTop[1]-ADD_PIXELS:extBot[1]+ADD_PIXELS,
+                      extLeft[0]-ADD_PIXELS:extRight[0]+ADD_PIXELS].copy()
+        set_new.append(new_img)
+
+    return np.array(set_new)
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/diabetes')
+def diabetes():
+    return render_template('diabetes.html')
+
+
+@app.route('/alzheimer')
+def alzheimer():
+    return render_template('alzheimer.html')
+
+@app.route("/chatbot1")
+def chatbot1():
+    return render_template("chatbot1.html")
+
+
+@app.route('/resultd', methods=['POST'])
+def resultd():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        nmv = float(request.form['nmv'])
+        tcp = float(request.form['tcp'])
+        eia = float(request.form['eia'])
+        thal = float(request.form['thal'])
+        op = float(request.form['op'])
+        mhra = float(request.form['mhra'])
+        age = float(request.form['age'])
+        print(np.array([nmv, tcp, eia, thal, op, mhra, age]).reshape(1, -1))
+        pred = heart_model.predict(
+            np.array([nmv, tcp, eia, thal, op, mhra, age]).reshape(1, -1))
+        return render_template('resultd.html', fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+
+@app.route('/resulta', methods=['GET', 'POST'])
+def resulta():
+    if request.method == 'POST':
+        print(request.url)
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        gender = request.form['gender']
+        age = request.form['age']
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('Image successfully uploaded and displayed below')
+            img = cv2.imread('static/uploads/'+filename)
+            img = cv2.resize(img, (176, 176))
+            img = img.reshape(1, 176, 176, 3)
+            img = img/255.0
+            pred = alzheimer_model.predict(img)
+            pred = pred[0].argmax()
+            print(pred)
+            if pred < 0.99:
+                pred = 0
+            else:
+                pred = 1
+            return render_template('resulta.html', filename=filename, fn=firstname, ln=lastname, age = age, r=pred, gender=gender)
+
+        else:
+            flash('Allowed image types are - png, jpg, jpeg')
+            return redirect('/')
+        
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return chatbot_response(userText)
+
+
+# No caching at all for API endpoints.
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
